59d3d43bf1a19aee272c750c600545f172d9f70a
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-06-27 07:00:12 -0300

    implementa testes no componente FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { FavoritePokemons } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+import pokemons from '../data';
+
+describe('Teste o componente FavoritePokemons', () => {
+  it(`Teste se é exibida na tela a mensagem "No favorite pokemon found",
+  caso a pessoa não tenha pokémons favoritos`, () => {
+    render(<FavoritePokemons />);
+
+    const title = screen.getByText(/No favorite pokemon found/i);
+
+    expect(title).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+    pokemons.forEach((pokemon) => {
+      const name = screen.getByText(pokemon.name);
+      expect(name).toBeInTheDocument();
+    });
+  });
+
+  it('Teste se nenhum card de pokémon é exibido, se ele não estiver favoritado', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const queryIdName = screen.queryByTestId('pokemon-name');
+    expect(queryIdName).not.toBeInTheDocument();
+  });
+});
