499a587559cb75638efcb17fbd65e50559632861
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-06-27 05:29:29 -0300

    cria arquivo renderWithRouter e refaz testes do componente App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,40 +1,80 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('Teste o componente App.', () => {
-  describe('Teste se os links contém os respectivos nomes', () => {
-    test('O primeiro link deve possuir o texto Home', () => {
-      render(
-        <MemoryRouter>
-          <App />
-        </MemoryRouter>,
-      );
-      const homeLink = screen.getByRole('link', { name: /Home/i });
-      expect(homeLink).toBeInTheDocument();
-    });
+  it('O primeiro link deve possuir o texto Home', () => {
+    renderWithRouter(<App />);
 
-    test('O segundo link deve possuir o texto About', () => {
-      render(
-        <MemoryRouter>
-          <App />
-        </MemoryRouter>,
-      );
-      const aboutLink = screen.getByRole('link', { name: /About/i });
-      expect(aboutLink).toBeInTheDocument();
-    });
+    const homeLink = screen.getByRole('link', { name: /Home/i });
+    expect(homeLink).toBeInTheDocument();
+  });
 
-    test('O terceiro link deve possuir o texto Favorite Pokémons', () => {
-      render(
-        <MemoryRouter>
-          <App />
-        </MemoryRouter>,
-      );
-      const favoritePokemonsLink = screen.getByRole('link', {
-        name: /Favorite Pokémons/i,
-      });
-      expect(favoritePokemonsLink).toBeInTheDocument();
+  it('O segundo link deve possuir o texto About', () => {
+    renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: /About/i });
+    expect(aboutLink).toBeInTheDocument();
+  });
+
+  it('O terceiro link deve possuir o texto Favorite Pokémons', () => {
+    renderWithRouter(<App />);
+
+    const favoritePokemonsLink = screen.getByRole('link', {
+      name: /Favorite Pokémons/i,
     });
+    expect(favoritePokemonsLink).toBeInTheDocument();
+  });
+
+  it(`Teste se a aplicação é redirecionada para a página inicial,
+        na URL / ao clicar no link Home`, () => {
+    const { history } = renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: /home/i });
+    expect(homeLink).toBeInTheDocument();
+    userEvent.click(homeLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it(`Teste se a aplicação é redirecionada para a página de About, 
+  na URL /about, ao clicar no link About`, () => {
+    const { history } = renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    expect(aboutLink).toBeInTheDocument();
+    userEvent.click(aboutLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it(`Teste se a aplicação é redirecionada para a página de Pokémons Favoritados,
+    na URL /favorites, ao clicar no link Favorite Pokémons da barra de navegação`, () => {
+    const { history } = renderWithRouter(<App />);
+
+    const favoriteLink = screen.getByRole('link', {
+      name: /Favorite Pokémons/i,
+    });
+    expect(favoriteLink).toBeInTheDocument();
+    userEvent.click(favoriteLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('deve testar um caminho não existente e a renderização do Not Found', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/pagina-que-nao-existe');
+
+    const notFoundTitle = screen.getByRole('heading', {
+      name: 'Page requested not found Crying emoji',
+      level: 2,
+    });
+    expect(notFoundTitle).toBeInTheDocument();
   });
 });
